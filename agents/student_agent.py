c030ac5ea5bc8a4d2fa1496fab67f983db7bfbdd
Rambod Azimi <59399542+rambodazimi@users.noreply.github.com>
2023-11-21 23:32:18 -0500

    added 5 heuristics

--- agents/student_agent.py
+++ agents/student_agent.py
@@ -12,6 +12,157 @@ from copy import deepcopy
 import time
 
 
+# Moves (Up, Right, Down, Left)
+moves = ((-1, 0), (0, 1), (1, 0), (0, -1))
+
+
+def number_of_allowed_dirs(chess_board, my_pos, adv_pos):
+    """
+    This method calculates the number of possible directions that the student agent can go and returns both the value and possible dirs as a list
+    """
+    row, col = my_pos
+    # Build a list of the moves we can make
+    allowed_dirs = [ d                                
+        for d in range(0, 4) # 4 moves possible (up, down, left, right)
+        if not chess_board[row, col, d] and # chess_board True means wall
+        not adv_pos == (row + moves[d][0], col + moves[d][1])] # cannot move through Adversary
+    return allowed_dirs, len(allowed_dirs)
+
+
+def total_allowed_moves(chess_board, my_pos, adv_pos, max_step, allowed_dirs):
+    """
+    This method use recursion to examine all the possible moves that student agent can go and returns the value
+    """
+    if(max_step == 0):
+        return 0
+    
+    allowed_dirs, _ = number_of_allowed_dirs(chess_board, my_pos, adv_pos)
+
+    if(len(allowed_dirs) == 0):
+        return 0
+    elif(len(allowed_dirs) == 1):
+        return 1 + total_allowed_moves(chess_board, moves[allowed_dirs[0]], adv_pos, max_step-1, allowed_dirs)
+    elif(len(allowed_dirs) == 2):
+        return 1 + total_allowed_moves(chess_board, moves[allowed_dirs[0]], adv_pos, max_step-1, allowed_dirs) + total_allowed_moves(chess_board, moves[allowed_dirs[1]], adv_pos, max_step-1, allowed_dirs)
+    elif(len(allowed_dirs) == 3):
+        return 1 + total_allowed_moves(chess_board, moves[allowed_dirs[0]], adv_pos, max_step-1, allowed_dirs) + total_allowed_moves(chess_board, moves[allowed_dirs[1]], adv_pos, max_step-1, allowed_dirs) + total_allowed_moves(chess_board, moves[allowed_dirs[2]], adv_pos, max_step-1, allowed_dirs)
+    elif(len(allowed_dirs) == 4):
+        return 1 + total_allowed_moves(chess_board, moves[allowed_dirs[0]], adv_pos, max_step-1, allowed_dirs) + total_allowed_moves(chess_board, moves[allowed_dirs[1]], adv_pos, max_step-1, allowed_dirs) + total_allowed_moves(chess_board, moves[allowed_dirs[2]], adv_pos, max_step-1, allowed_dirs) + total_allowed_moves(chess_board, moves[allowed_dirs[3]], adv_pos, max_step-1, allowed_dirs)
+    else:
+        return 0
+    
+
+def manhattan_distance(my_pos, adv_pos):
+    row_A, col_A = my_pos
+    row_B, col_B = adv_pos
+
+    return abs(row_A - row_B) + abs(col_A - col_B)
+
+
+def distance_from_nearest_corner(chess_board, my_pos, adv_pos):
+    """
+    This method calculates the distance from the closest corner for both the player (student agent) and opponent (random agent) and returns a tuple
+    """
+    length = chess_board.shape[0]
+    top_left_corner = (0, 0)
+    bottom_left_corner = (chess_board.shape[0] - 1, 0)
+    top_right_corner = (0, chess_board.shape[1] - 1)
+    bottom_right_corner = (chess_board.shape[0] - 1, chess_board.shape[1] - 1)
+
+    x_A, y_A = my_pos
+    x_B, y_B = adv_pos
+
+    mid = int(length / 2)
+
+    # student agent
+    if(x_A < mid):
+        if(y_A < mid):
+            # top left
+            distance_A = manhattan_distance(top_left_corner, my_pos)
+        else:
+            # top right
+            distance_A = manhattan_distance(top_right_corner, my_pos)
+    else:
+        if(y_A < mid):
+            # bottom left
+            distance_A = manhattan_distance(bottom_left_corner, my_pos)
+        else:
+            # bottom right
+            distance_A = manhattan_distance(bottom_right_corner, my_pos)
+
+    # random agent
+    if(x_B < mid):
+        if(y_B < mid):
+            # top left
+            distance_B = manhattan_distance(top_left_corner, adv_pos)
+        else:
+            # top right
+            distance_B = manhattan_distance(top_right_corner, adv_pos)
+    else:
+        if(y_B < mid):
+            # bottom left
+            distance_B = manhattan_distance(bottom_left_corner, adv_pos)
+        else:
+            # bottom right
+            distance_B = manhattan_distance(bottom_right_corner, adv_pos)
+
+    return distance_A, distance_B
+
+
+def min_max_normalize(value, min_val, max_val):
+    """
+    This method applies the min max normalization to normalize the heuristic scores
+    """
+    normalized_value = (value - min_val) / (max_val - min_val)
+    return normalized_value
+
+
+def calculate_score(chess_board, my_pos, adv_pos, max_step):
+    """
+    This method is used for calculating the total score at each turn based on different heuristics:
+    1. Number of allowed directions
+       At each turn, calculate the number of possible directions that student agent can choose to move (0 to 4)
+
+    2. Total possible moves
+       At each turn, calculate the total number of possible moves the student agent can move (approximately from 1 to M^2)
+
+    3. Manhattan Distance
+       At each turn, calculate the manhattan distance from the student agent to the random agent (approximately 1 to 2*M)
+
+    4. Distance from the nearest corner (student agent)
+
+    5. Distance from the nearest corner (random agent)
+
+    Finally, we apply min max normalization technique and choose weights to prioritize some heuristics and return the final score
+    (For example, number of allowed directions seems to be more important than the distance from the opponent; thus, we consider a higher weight for that) 
+    """
+    _, heuristic_1 = number_of_allowed_dirs(chess_board, my_pos, adv_pos)
+    print(f"Number of allowed dirs for student agent: {heuristic_1}")
+
+    heuristic_2 = 0
+    for i in range(0, max_step+1):
+        heuristic_2 += total_allowed_moves(chess_board, my_pos, adv_pos, i, [])
+    print(f"Total number of allowed moves for student agent: {heuristic_2}")
+
+    heuristic_3 = manhattan_distance(my_pos, adv_pos)
+    print(f"Manhattan distance between A and B: {heuristic_3}")
+
+    heuristic_4, heuristic_5 = distance_from_nearest_corner(chess_board, my_pos, adv_pos)
+    print(f"Player A distance from the nearest corner: {heuristic_4}")
+    print(f"Player B distance from the nearest corner: {heuristic_5}")
+
+    # calculating the total score
+    normalized_heuristic_1 = min_max_normalize(heuristic_1, 0, 4)
+    normalized_heuristic_2 = min_max_normalize(heuristic_2, 0, chess_board.shape[0] ** 2)
+    normalized_heuristic_3 = min_max_normalize(heuristic_3, 1, chess_board.shape[0] * 2)
+    normalized_heuristic_4 = min_max_normalize(heuristic_4, 0, chess_board.shape[0])
+    normalized_heuristic_5 = min_max_normalize(heuristic_5, 0, chess_board.shape[0])
+    weights = [0.4, 0.2, 0.2, 0.1, 0.1]
+    total_score = normalized_heuristic_1 * weights[0] + normalized_heuristic_2 * weights[1] + normalized_heuristic_3 * weights[2] + normalized_heuristic_4 * weights[3] - normalized_heuristic_5 * weights[4]
+
+    return total_score * 100
+
+
 @register_agent("student_agent")
 class StudentAgent(Agent):
     """
@@ -44,13 +195,14 @@ class StudentAgent(Agent):
 
         Please check the sample implementation in agents/random_agent.py or agents/human_agent.py for more details.
         """
-
-        # Some simple code to help you with timing. Consider checking 
-        # time_taken during your search and breaking with the best answer
-        # so far when it nears 2 seconds.
         start_time = time.time()
-        time_taken = time.time() - start_time
+
+        total_score = calculate_score(chess_board, my_pos, adv_pos, max_step)
+        print(f"TOTAL SCORE: {total_score:.2f}")
+
         
+
+        time_taken = time.time() - start_time
         print("My AI's turn took ", time_taken, "seconds.")
 
         # dummy return
