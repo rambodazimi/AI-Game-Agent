c1f08ea2d07af1c3e990f2a46266ef2fcee4c2fd
Matthew Spagnuolo <matthew.spagnuolo@mail.mcgill.ca>
2023-11-26 00:04:22 -0500

    Changed allowed moves to allow omitting dirs

--- agents/__init__.py
+++ agents/__init__.py
@@ -2,3 +2,4 @@ from .agent import Agent
 from .random_agent import RandomAgent
 from .human_agent import HumanAgent
 from .student_agent import StudentAgent
+from .student_agent_3 import StudentAgent3
